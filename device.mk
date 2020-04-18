77e9f2060dadff053ef9e0983bd3d4272a5a3fce
Dušan Uverić <dusan.uveric9@gmail.com>
2020-04-18 19:51:36 +0200

    jason: drop lineagehw
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- device.mk
+++ device.mk
@@ -111,10 +111,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
-# Lineage hardware
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.jason
-
 # HW crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
